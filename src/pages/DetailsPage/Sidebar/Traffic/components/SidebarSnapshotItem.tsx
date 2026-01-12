0974d06a87317ab05c04142001e1b8ee3e0b1a9e
muramets <97661598+muramets@users.noreply.github.com>
2026-01-12 07:32:54 +0200

    Standardize sidebar indentation hierarchy steps

--- src/pages/DetailsPage/Sidebar/Traffic/components/SidebarSnapshotItem.tsx
+++ src/pages/DetailsPage/Sidebar/Traffic/components/SidebarSnapshotItem.tsx
@@ -100,7 +100,7 @@ export const SidebarSnapshotItem: React.FC<SidebarSnapshotItemProps> = ({
 
             {/* Render Niches if this snapshot is selected */}
             {isSelected && hasNiches && nicheImpressions && groups && (
-                <div className="ml-12 pl-4 mb-1 pr-3 mt-1">
+                <div className="ml-12 mb-1 pr-3 mt-1">
                     <TrafficSidebarNicheList
                         nicheImpressions={nicheImpressions}
                         groups={groups}
